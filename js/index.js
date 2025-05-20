d7cbdca82d2686458514c25f85f5fd1221f3da12
Loïc <nephtyse19@hotmail.fr>
2025-05-20 11:46:38 +0200

    fix(carousel) add missing semicolon to carousel variable declaration

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@
 let images = ["Carousel1.jpg","Carousel2.jpg","Carousel3.jpg","Carousel4.jpg","Carousel5.jpg"];
 let current = 0;
 
-let carousel = document.getElementsByClassName("carousel")
+let carousel = document.getElementsByClassName("carousel");
