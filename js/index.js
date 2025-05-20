64c0ccd1158cbc9c7edc83d6a5a6e8dbca2da409
Loïc <nephtyse19@hotmail.fr>
2025-05-20 12:18:31 +0200

    refactor(carousel) move variable declarations for carousel and buttons to the top

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 
 let images = ["Carousel1.jpg","Carousel2.jpg","Carousel3.jpg","Carousel4.jpg","Carousel5.jpg"];
 let current = 0;
-
 let carousel = document.getElementsByClassName("carousel");
+let boutonNext = document.getElementById("next");
+let boutonPrev = document.getElementById("prev")
+
+
+document.getElementById("")
