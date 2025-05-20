3927363b3cb04f5e090ab702e8fae8c2617646fb
Loïc <nephtyse19@hotmail.fr>
2025-05-20 11:46:33 +0200

    refactor(carousel) reorganize image and carousel variable declarations

--- js/index.js
+++ js/index.js
@@ -1,2 +1,5 @@
-let buttons = document.querySelectorAll(".btn")
-console.log(buttons)
+
+let images = ["Carousel1.jpg","Carousel2.jpg","Carousel3.jpg","Carousel4.jpg","Carousel5.jpg"];
+let current = 0;
+
+let carousel = document.getElementsByClassName("carousel")
